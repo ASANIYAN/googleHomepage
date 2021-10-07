21a17094a9feaa1bebe595d520187cb695d3604d
Asaniyan Ayomide <heritageayomide2017@gmail.com>
2021-10-07 02:15:40 +0100

    created a right-side-nav and complted the implementation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let hamburger = document.querySelector('#nav-svg');
 let sideBar = document.querySelector('#sidebar');
-let overlay = document.querySelector('#overlay')
+let overlay = document.querySelector('#overlay');
+let svg = document.querySelector('#svg');
+let extra = document.querySelector('#extras');
+let body = document.querySelector('#body');
 
 hamburger.addEventListener('click', function() {
     sideBar.style.transform = 'translateX(250px)';
@@ -11,3 +14,16 @@ overlay.addEventListener('click', function() {
     sideBar.style.transform = "translateX(0)";
     overlay.classList.remove('overlay');
 })
+
+svg.addEventListener('click', function() {
+    extra.classList.remove('visibility');
+})
+
+body.addEventListener('click', function(e) {
+    if (e.target.classList.contains('svg')) {
+        extra.classList.remove('visibility');
+    } else {
+        extra.classList.add('visibility')
+    }
+})
+
